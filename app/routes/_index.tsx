1d5fe9d51b422d99505b4a978c15d29bde74409d
Shirahamah <hiropi1224s@gmail.com>
2023-10-01 15:46:10 +0900

    ➕ add mantine

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,61 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
+import { List, ThemeIcon, Title } from '@mantine/core';
+import { IconCircleCheck, IconCircleDashed } from '@tabler/icons-react';
+import '@mantine/core/styles.css';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <Title order={1} c='blue'>
+        Welcome to Remix
+      </Title>
+      <List
+        spacing='xs'
+        size='sm'
+        center
+        icon={
+          <ThemeIcon color='teal' size={24} radius='xl'>
+            <IconCircleCheck size='1rem' />
+          </ThemeIcon>
+        }
+      >
+        <List.Item>
           <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
+            target='_blank'
+            href='https://remix.run/tutorials/blog'
+            rel='noreferrer'
           >
             15m Quickstart Blog Tutorial
           </a>
-        </li>
-        <li>
+        </List.Item>
+        <List.Item>
           <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
+            target='_blank'
+            href='https://remix.run/tutorials/jokes'
+            rel='noreferrer'
           >
             Deep Dive Jokes App Tutorial
           </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+        </List.Item>
+        <List.Item
+          icon={
+            <ThemeIcon color='blue' size={24} radius='xl'>
+              <IconCircleDashed size='1rem' />
+            </ThemeIcon>
+          }
+        >
+          <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
             Remix Docs
           </a>
-        </li>
-      </ul>
+        </List.Item>
+      </List>
     </div>
   );
 }

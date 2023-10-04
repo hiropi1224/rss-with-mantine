28ac5d1347de25fed8cb9b8326373880a0019cf5
Shirahamah <hiropi1224s@gmail.com>
2023-10-04 22:59:50 +0900

    🩹 fix

--- app/routes/top.$id.tsx
+++ app/routes/top.$id.tsx
@@ -1,3 +1,4 @@
+import { Paper, Stack, Title, Text } from '@mantine/core';
 import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import { getItem } from '~/utils/hackerNews.server';
@@ -17,23 +18,28 @@ export default function Item() {
   const { item, kids } = useLoaderData<typeof loader>();
 
   return (
-    <article>
-      <h1>{item.title}</h1>
-      <p>
-        by {item.by} on {new Date(item.time * 1000).toLocaleString()}
-      </p>
-      <p>
-        <a href={item.url}>{item.url}</a>
-      </p>
-      <h2>Comments</h2>
-      {kids.map((kidsItem) => (
-        <div key={kidsItem.id}>
-          <h3>by: {kidsItem.by}</h3>
-          <p>{kidsItem.text}</p>
-          <p>{new Date(kidsItem.time * 1000).toLocaleString()}</p>
-          <hr />
-        </div>
-      ))}
-    </article>
+    <Stack>
+      <Stack>
+        <Title order={1}>{item.title}</Title>
+        <Text>
+          by {item.by} on {new Date(item.time * 1000).toLocaleString()}
+        </Text>
+        <Text>
+          <a href={item.url}>{item.url}</a>
+        </Text>
+      </Stack>
+      <Title order={2}>Comments</Title>
+      <Stack gap='xs'>
+        {kids.map((kidsItem) => (
+          <Paper key={kidsItem.id} shadow='xs' p='sm'>
+            <Title order={3}>by: {kidsItem.by}</Title>
+            <Text p='md'>{kidsItem.text}</Text>
+            <Text px='md'>
+              {new Date(kidsItem.time * 1000).toLocaleString()}
+            </Text>
+          </Paper>
+        ))}
+      </Stack>
+    </Stack>
   );
 }
